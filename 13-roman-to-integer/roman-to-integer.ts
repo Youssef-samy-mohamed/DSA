6f59f6eb89654143c1968b7346c1ced320fb24de
Youssef Samy <samyyoussef813@gmail.com>
2025-04-11 16:30:41 +0200

    Time: 5 ms (68.13%) | Memory: 61.8 MB (60.37%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -0,0 +1,18 @@
+function romanToInt(s: string): number {
+    const roman = { 'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        };
+    let sum = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = roman[s[i]];
+        const next = roman[s[i+1]];
+        sum += (next > current) ? -current : current; // Compact logic
+    }
+    return sum;
+}
