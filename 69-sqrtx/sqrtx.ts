b506b6dca1bf5e64f9cf966a576330fe7373009d
Youssef Samy <samyyoussef813@gmail.com>
2025-04-20 15:49:10 +0200

    Time: 1 ms (74.71%) | Memory: 57.3 MB (87.16%) - LeetSync

---
+++ 69-sqrtx/sqrtx.ts
@@ -0,0 +1,37 @@
+function mySqrt(x: number): number {
+
+    //  am solving it with binary search approach 
+    if ( x < 2 ) return x;
+
+
+    let left = 1;
+    let right = x;
+    let result = 0;
+
+
+    while ( left <= right ){
+
+        const mid = Math.floor((left + right ) / 2 )
+        const square = mid * mid;
+
+        if ( square === x ){
+            return mid;
+
+        }else if ( square < x ){
+            result = mid;
+            left = mid + 1;
+
+        }else {
+            right = mid - 1;
+        }
+
+
+
+
+    }
+
+
+
+return result 
+    
+};
