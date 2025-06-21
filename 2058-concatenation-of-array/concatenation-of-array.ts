ba92e1b82cc8f868af38ac651fb454cd82e06715
Youssef Samy <samyyoussef813@gmail.com>
2025-06-21 17:02:10 +0300

    Time: 0 ms (100.00%) | Memory: 60.3 MB (23.30%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.ts
@@ -0,0 +1,11 @@
+function getConcatenation(nums: number[]): number[] {
+    
+    let ans:number [] = [];
+    let n:number = nums.length;
+
+    for ( let i= 0 ; i < nums.length ; i++){
+        ans[i] = nums[i] 
+        ans[i + n] = nums[i]
+    }
+    return ans;
+};
