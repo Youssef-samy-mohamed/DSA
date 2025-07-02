7622cd7da68227133cba9f139d5481fb73f1488c
Youssef Samy <samyyoussef813@gmail.com>
2025-07-03 02:16:49 +0300

    Time: 11 ms (45.04%) | Memory: 65.6 MB (56.04%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.ts
@@ -0,0 +1,36 @@
+function merge(intervals: number[][]): number[][] {
+    // let's define the problem 
+    // we need to merge the overlapping intervals 
+
+    // analyze the problem 
+    // so if the number in the start or the end of the intervel or between them we merge if not we skip 
+    
+    // generate time 
+    
+
+    if ( intervals.length <= 1 ) return intervals;
+
+//  sort the fisrt element in the array 
+    intervals.sort(( a , b ) => a[0] - b[0])
+
+    const merged: number[][] = [];
+    let current = intervals[0]
+
+
+    for( let i = 1 ; i < intervals.length ; i++ ) {
+        const next = intervals[i]
+        if ( current[1] >= next[0]){
+            // merge 
+            current[1] = Math.max(current[1] , next[1])
+        }else {
+            // if there's no ovelap
+            merged.push(current)
+            current = next;
+        }
+    }
+// push last array 
+merged.push(current)
+
+return merged;
+    
+};
